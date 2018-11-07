109b54810edb1738ce02c852eeeb06753b57b442
wliu6 <1029769711@qq.com>
2018-11-07 15:35:31 +0800

    dev

--- lib/fastlane/plugin/bugtags/actions/bugtags_action.rb
+++ lib/fastlane/plugin/bugtags/actions/bugtags_action.rb
@@ -6,7 +6,35 @@ module Fastlane
     class BugtagsAction < Action
       def self.run(params)
         UI.message("The bugtags plugin is working!")
+        command = []
+        command << "curl"
+        command << "\"https://work.bugtags.com/api/apps/symbols/upload\""
+        command << "--write-out \"%{http_code}\" --silent"
+        command += handle_params(params)
+        command += handle_plistinfo(params)
+        shell_command = command.join(' ')
+        result = Actions.sh(shell_command)
+        return result
       end
+
+      def self.handle_params (params)
+        result = []
+        result << "-F \"file=@#{params[:dsym].shellescape};type=application/octet-stream\""
+        result << "-F \"app_key=#{params[:app_key].shellescape}\""
+        result << "-F \"secret_key=#{params[:secret_key].shellescape}\""
+        return result
+      end
+
+      def self.handle_plistinfo (params)
+        result = []
+        result << "-F \"version_name=#{params[:app_version].shellescape}\""
+        result << "-F \"version_code=#{params[:app_build].shellescape}\""
+        return result
+      end
+
+      #####################################################
+      # @!group Documentation
+      #####################################################
 
       def self.description
         "Upload symbols to Bugtags"
@@ -27,20 +55,48 @@ module Fastlane
 
       def self.available_options
         [
-          # FastlaneCore::ConfigItem.new(key: :your_option,
-          #                         env_name: "BUGTAGS_YOUR_OPTION",
-          #                      description: "A description of your option",
-          #                         optional: false,
-          #                             type: String)
+          FastlaneCore::ConfigItem.new(key: :dsym,
+                                      env_name: "FL_BUGTAGS_DSYM_FILE", 
+                                      description: "the dSYM.zip file to upload to Bugtags", 
+                                      verify_block: proc do |value|
+                                        UI.user_error!("No dsym for BugtagsAction given, pass using `dsym: 'dsym file's path'`") unless (value and not value.empty?)
+                                        # UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
+                                      end),
+          FastlaneCore::ConfigItem.new(key: :app_version,
+                                      env_name: "FL_BUGTAGS_APP_VERSION",
+                                      description: "the version in your project's info.plist",
+                                      verify_block: proc do |value|
+                                        UI.user_error!("No version for BugtagsAction given, pass using `dsym: 'app_version file's path'`") unless (value and not value.empty?)
+                                        # UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
+                                      end),
+          FastlaneCore::ConfigItem.new(key: :app_build,
+                                      env_name: "FL_BUGTAGS_APP_BUILD",
+                                      description: "the build version in your project's info.plist",
+                                      verify_block: proc do |value|
+                                        UI.user_error!("No build version for BugtagsAction given, pass using `app_build: 'build version'`") unless (value and not value.empty?)
+                                        # UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
+                                      end),
+          FastlaneCore::ConfigItem.new(key: :app_key,
+                                      env_name: "FL_BUGTAGS_APP_KEY",
+                                      description: "Bugtags App ID key e.g. 29f35c87cb99e10e00c7xxxx",
+                                      verify_block: proc do |value|
+                                        UI.user_error!("No App ID for BugtagsAction given, pass using `app_key: 'Bugtags App ID key'`") unless (value and not value.empty?)
+                                        # UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
+                                      end),
+          FastlaneCore::ConfigItem.new(key: :secret_key,
+                                      env_name: "FL_BUGTAGS_SECRET_KEY",
+                                      description: "Bugtags App Secret key e.g. 29f35c87cb99e10e00c7xxxx",
+                                      verify_block: proc do |value|
+                                        UI.user_error!("No App Secret for BugtagsAction given, pass using `secret_key: 'Bugtags App Secret key'`") unless (value and not value.empty?)
+                                        # UI.user_error!("Couldn't find file at path '#{value}'") unless File.exist?(value)
+                                      end)
+                                      #is_string: false, # true: verifies the input is a string, false: every kind of value
+                                      #default_value: false) # the default value if the user didn't provide one
         ]
       end
 
       def self.is_supported?(platform)
-        # Adjust this if your plugin only works for a particular platform (iOS vs. Android, for example)
-        # See: https://docs.fastlane.tools/advanced/#control-configuration-by-lane-and-by-platform
-        #
-        # [:ios, :mac, :android].include?(platform)
-        true
+        platform == :ios
       end
     end
   end
